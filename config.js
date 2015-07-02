b0f6cff2624987f17a8b5a658fd2a12ddad4ffb0
Matt Duncan <busman84@gmail.com>
2015-07-02 09:19:30 -0500

    getting heroku to work

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {};
 
-config.mongoUri = 'mongodb://localhost:27017/GOAT';
+config.mongoUri = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/GOAT';
 config.cookieMaxAge = 21 * 24 * 3600 * 1000;
 
 module.exports = config;
